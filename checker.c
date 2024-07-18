5c43d1100739f7cf4fa0afd0ab023a183b4a0d9f
Ganesha A S <93441611+Ganesha1234@users.noreply.github.com>
2024-07-18 18:19:44 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -86,7 +86,7 @@ int batteryIsOk(float temperature, float soc, float chargeRate)
     //int crIsOk = chargeRateIsOk(chargeRate);
     //int result = tempIsOk&stcIsOk&crIsOk;
     //return result;
-     return 0;
+     return 1;
 }
 int main() 
 {
