0bfa8e080c2a86f93d63967811f7016967fabb8c
Ganesha A S <93441611+Ganesha1234@users.noreply.github.com>
2024-07-31 19:46:35 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,46 +1,105 @@
 #include <stdio.h>
-#include <assert.h>  
-int TemperaureIsOk(float temperature)
-{
-     if (temperature < 0 || temperature > 45) 
-     {
-        printf("Temperature out of range\n");
-        return 0;
-     }
-     return 1;
-}
-int socIsOk(float soc)
-{
-    if (soc < 20 || soc > 80) 
-    {
-        printf("State of Charge out of range!\n");
-        return 0;
-    }
-    return 1;
-}
-int chargeRateIsOk(float chargeRate)
-{
-    if (chargeRate > 0.8) 
-    {
-        printf("Charge Rate out of range!\n");
-        return 0;
-    }
-    return 1;
-}
- 
- 
- 
-int CheckbatteryIsOk(float temperature, float soc, float chargeRate) 
-{
-    int tempIsOk = TemperaureIsOk(temperature);
-    int stcIsOk = socIsOk(soc);
-    int crIsOk = chargeRateIsOk(chargeRate);
-    int result = tempIsOk&stcIsOk&crIsOk;
-    return result;
-}
-int main() 
-{
-  assert(CheckbatteryIsOk(25, 70, 0.7));
-  assert(!CheckbatteryIsOk(50, 85, 0));
+#include <assert.h>
+#include <string.h>
+
+#define TOLERANCE_PERCENTAGE 0.05
+
+
+#define TEMP_MIN  0
+#define TEMP_MAX  45
+#define SOC_MIN  20
+#define SOC_MAX  80
+#define CHARGE_RATE_MAX  0.8
+
+typedef enum {
+    ENGLISH,
+    GERMAN
+} Language;
+
+Language currentLanguage = ENGLISH;
+
+const char* TemperatureMessages[2][3] = {
+    {"Temperature out of range!", "Warning: Approaching low temperature", "Warning: Approaching high temperature"},
+    {"Temperatur außerhalb des Bereichs!", "Warnung: Annäherung an niedrige Temperatur", "Warnung: Annäherung an hohe Temperatur"}
+};
+
+const char* SoCMessages[2][3] = {
+    {"State of Charge out of range!", "Warning: Approaching discharge", "Warning: Approaching charge-peak"},
+    {"Ladezustand außerhalb des Bereichs!", "Warnung: Annäherung an Entladung", "Warnung: Annäherung an Ladungsspitze"}
+};
+
+const char* ChargeRateMessages[2][1] = {
+    {"Charge Rate out of range!"},
+    {"Laderate außerhalb des Bereichs!"}
+};
+
+const char*  printMessage(const char* message) {
+    printf("%s\n", message);
 }
 
+float calculateTolerance(float upperLimit) {
+    return upperLimit * TOLERANCE_PERCENTAGE;
+}
+
+int checkTemperature(float temperature) {
+    float lowerWarningLimit = TEMP_MIN + calculateTolerance(TEMP_MAX);
+    float upperWarningLimit = TEMP_MAX - calculateTolerance(TEMP_MAX);
+    
+    if (temperature < TEMP_MIN || temperature > TEMP_MAX) {
+        printMessage(TemperatureMessages[currentLanguage][0]);
+        return 0;
+    }
+    if (temperature <= lowerWarningLimit) {
+        printMessage(TemperatureMessages[currentLanguage][1]);
+    }
+    if (temperature >= upperWarningLimit) {
+        printMessage(TemperatureMessages[currentLanguage][2]);
+    }
+    return 1;
+}
+
+int checkSoC(float soc) {
+    float lowerWarningLimit = SOC_MIN + calculateTolerance(SOC_MAX);
+    float upperWarningLimit = SOC_MAX - calculateTolerance(SOC_MAX);
+
+    if (soc < SOC_MIN || soc > SOC_MAX) {
+        printMessage(SoCMessages[currentLanguage][0]);
+        return 0;
+    }
+    if (soc <= lowerWarningLimit) {
+        printMessage(SoCMessages[currentLanguage][1]);
+    }
+    if (soc >= upperWarningLimit) {
+        printMessage(SoCMessages[currentLanguage][2]);
+    }
+    return 1;
+}
+
+int checkChargeRate(float chargeRate) {
+    if (chargeRate > CHARGE_RATE_MAX) {
+        printMessage(ChargeRateMessages[currentLanguage][0]);
+        return 0;
+    }
+    return 1;
+}
+
+int checkBatteryIsOk(float temperature, float soc, float chargeRate) {
+    int tempIsOk = checkTemperature(temperature);
+    int socIsOk = checkSoC(soc);
+    int crIsOk = checkChargeRate(chargeRate);
+    return tempIsOk && socIsOk && crIsOk;
+}
+
+int main() {
+    currentLanguage = ENGLISH;  // Change to GERMAN for German messages
+    
+    assert(checkBatteryIsOk(25, 70, 0.7));
+    assert(!checkBatteryIsOk(50, 85, 0));
+    
+    
+    currentLanguage = GERMAN;  // Switch to German
+    assert(checkBatteryIsOk(25, 70, 0.7));
+    assert(!checkBatteryIsOk(50, 85, 0));
+
+    return 0;
+}
